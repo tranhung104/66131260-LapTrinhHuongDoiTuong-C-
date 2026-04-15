d47d9d197a551bef0a0eb1ad439fb9aeac4d62dc
tranhung104 <tranhungvip01@gmail.com>
2026-04-15 11:36:04 +0700

    Update ElectricityBill.cs

--- ElectricityBill.cs
+++ ElectricityBill.cs
@@ -17,24 +17,42 @@ namespace TinhTienDien
 {
     class Program
     {
-        // Khai báo các hằng 
-        const int Muc1=100, Muc2=150, Muc3=300;
-        const int Gia1=2000, Gia2=2500, Gia3=3000;
+        const int Muc1 = 100, Muc2 = 150, Muc3 = 300;
+        const int Gia1 = 2000, Gia2 = 2500, Gia3 = 3000;
 
         static void Main()
         {
-            int sokWh=0;
-            double sotien=0;
+            int sokWh = 0;
+            double sotien = 0;
 
             // Input
-            // Viết code của bạn để xử lý nhập số kWh tiêu thụ 
-            
+            Console.Write("Nhap so kWh tieu thu: ");
+            sokWh = int.Parse(Console.ReadLine());
+
             // Process
-            // Viết code của bạn để tính số tiền vào biến sotien
-            // Chỉ tính số tiền, không in ra kết quả ở phần này
+            if (sokWh <= Muc1)
+            {
+                sotien = sokWh * Gia1;
+            }
+            else if (sokWh <= Muc2)
+            {
+                sotien = Muc1 * Gia1
+                       + (sokWh - Muc1) * Gia2;
+            }
+            else
+            {
+                sotien = Muc1 * Gia1
+                       + (Muc2 - Muc1) * Gia2
+                       + (sokWh - Muc2) * Gia3;
+            }
+
+            if (sokWh > Muc3)
+            {
+                sotien *= 1.1;
+            }
 
             // Output
-            Console.WriteLine("So tien: {0}",sotien);
+            Console.WriteLine("So tien: {0}", sotien);
         }
     }
 }
