596e4b385cc3b68cbd3d19cb862eece2c816965e
tranhung104 <tranhungvip01@gmail.com>
2026-04-15 11:43:01 +0700

    Add files via upload
    Một loại vi trùng cứ sau mỗi giờ lại nhân đôi. Hỏi ban đầu có n con vi trùng thì sau h giờ số lượng là bao nhiêu?
    
    Input:
    - Số lượng vi trùng ban đầu (con)
    - Khoảng thời gian (giờ).
    
    Output: Số lượng vi trùng sau khoảng thời gian đã cho.

---
+++ vitrung.cs
@@ -0,0 +1,9 @@
+/*
+Một loại vi trùng cứ sau mỗi giờ lại nhân đôi. Hỏi ban đầu có n con vi trùng thì sau h giờ số lượng là bao nhiêu?
+
+Input:
+- Số lượng vi trùng ban đầu (con)
+- Khoảng thời gian (giờ).
+
+Output: Số lượng vi trùng sau khoảng thời gian đã cho.
+*/
